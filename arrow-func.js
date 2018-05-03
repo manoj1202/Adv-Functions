68357258ce701f9807480c6941edcbe0df9844ee
manoj1202 <manoj.appalaneni@gmail.com>
2018-05-02 23:40:05 -0500

    arrow Function

--- arrow-func.js
+++ arrow-func.js
@@ -1,2 +1,16 @@
 const square = (num) => num * num;
 console.log(square(5)); 
+
+const people = [{
+    name : 'Nick',
+    age : 24
+},{
+    name : 'vikram',
+    age : 31
+},{
+    name : "jen",
+    age : 22
+}]
+
+const under30 = people.filter((person) => person.age <30);
+console.log(under30) 
