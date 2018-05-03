1f6f03b52a55fb6217a03c4958e3aa6a13c3abc1
manoj1202 <manoj.appalaneni@gmail.com>
2018-05-03 12:46:58 -0500

    Conditional operator

--- conditional-operator.js
+++ conditional-operator.js
@@ -3,6 +3,6 @@ const team = ['Tyler','Potter'];
 
 team.push('Vikram', 'Rajesh');
 team.push('smith');
-const size = (team.length <= 4) ? `Team size: ${team.length}` : "Tomany people on your team"
+const size = (team.length <= 4) ? `Team size: ${team.length}` : "To many people on your team"
 
 console.log(size);
